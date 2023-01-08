5abaff36624884626d5649d365a3331e7e13d9ca
bratik1744 <stukundaniil@gmail.com>
2023-01-08 09:32:25 +0300

    08.01.2023 внос данных в бд, дополнения в код игры

--- sfk.py
+++ sfk.py
@@ -1,4 +1,5 @@
-a = int(input())
-b = -a
-print((-2 * a ** 2 - 2 * b ** 2) ** 2)
-print(4 * (a + b) * (a ** 3 + b ** 3))
+import sys
+
+sys.path.insert(0, r"C:\Users\stuku\PycharmProjects\the_yandex_app_2_pygame_project")
+
+exec('import escape')
